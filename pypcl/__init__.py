0ba33f617143bde6a517c2f78eddef8cb247d43f
Victor Lu <382441+victoriousluser@users.noreply.github.com>
2018-02-14 12:09:25 -0600

    When running on Windows, pypcl/__init__.py now adds libs folder path to PATH environment variable.

--- pypcl/__init__.py
+++ pypcl/__init__.py
@@ -1,3 +1,22 @@
+import os
+import os.path
+import sys
+import platform
+
+_root_dir = os.path.dirname(os.path.realpath(__file__))
+
+# manual specify list of dll directories, with paths relative to _root_dir
+_dll_dirs = ['libs']
+
+if platform.system() == 'Windows':
+    os.environ.setdefault('PATH','')
+    paths = os.environ['PATH'].split(';')
+    for x in _dll_dirs:
+        x = os.path.join(_root_dir, x)
+        if os.path.isdir(x) and x not in paths:
+            paths = [x] + paths
+    os.environ['PATH'] = ';'.join(paths)
+
 from viewer import *
 from points import *
 from kdtree import kdtree
